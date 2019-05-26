f17611bc47dcd7b6b2885b0e909677b443b0d783
leofranqui <leomfranqui@hotmail.com>
2019-05-26 19:22:09 -0300

    programa consegue cadastrar uma disciplina, excluir uma disciplina e visualizar as disciplinas cadastradas. Esse programa tem uma conexao com o banco de dados, que esta sem proteçao até o momento por ser um projeto teste.

---
+++ src/projectcourse/ProjectCourso.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectcourse;
+
+//******************************************************
+
+//Instituto Federal de São Paulo - Campus Sertãozinho
+
+//Disciplina......: M3LPBD
+
+//Programação de Computadores e Dispositivos Móveis
+
+//Aluno...........: Leonardo Thomaz Franqui
+
+//******************************************************
+
+/**
+ *
+ * @author Léo Franqui
+ */
+public class ProjectCourso {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //abre a tela inicial do programa
+        
+        new TelaInicial().setVisible(true);
+        
+       
+        // TODO code application logic here
+    }
+   
+}
